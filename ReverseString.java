c6834cf6e163bc3c092febf41b4a063aba46dfb2
Megha <chakrabortyreshmi077@gmail.com>
2024-06-04 20:01:05 +0530

    string reversed

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+//write a program to reverse a string
+import java.util.Scanner;
+class ReverseString{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        for(int i = 1; i <= input.length(); i++){
+           System.out.print(input.charAt(input.length() - i ));
+             
+        }
+    }
+}
