e2b509756bed0187d16d9ccfb838da9a9b523a46
Megha <chakrabortyreshmi077@gmail.com>
2024-06-05 11:15:34 +0530

    reversed string stored in a variable

--- IndependentDigit.java
+++ IndependentDigit.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 class IndependentDigit{
      public static void main(String [] args){
          Scanner sc = new Scanner(System.in);
-         int n = sc.nextInt();
-         String input = Integer.toString(n);
-         for(int i = 0; i < input.length(); i ++){
-            System.out.println(input.charAt(i));
+         int  n = sc.nextInt();
+         for(int i = 0; i < n; i ++){
+            System.out.println(n.getInteger(i));
     }
    } 
  }
