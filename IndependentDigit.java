236f519cd65e79026b2a13abee41be4c6d269bbb
Megha <chakrabortyreshmi077@gmail.com>
2024-06-05 22:34:55 +0530

    independent digits without string

--- IndependentDigit.java
+++ IndependentDigit.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 class IndependentDigit{
      public static void main(String [] args){
          Scanner sc = new Scanner(System.in);
-         int  n = sc.nextInt();
-         for(int i = 0; i < n; i ++){
-            System.out.println(Integer.getInteger(n));
-    }
-   } 
+         int n = sc.nextInt();  
+         int result;
+        while(n > 0){
+          result = n % 10;
+         System.out.println(result);
+          n = n/10;
+        }
+    } 
  }
